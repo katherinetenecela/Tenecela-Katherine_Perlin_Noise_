3a2c23976c5ddea5c16cef624e1558168b818836
KATHERINE YASBETH TENECELA MEJIA <katherine.tenecela@ucuenca.edu.ec>
2023-10-26 14:47:04 -0500

    TAREA Proyecyo con Perlin Noise

--- sketch.js
+++ sketch.js
@@ -1,28 +1,30 @@
-let t;
+let noiseTime = 0;
+let noiseTam = 80;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(200);
-  stroke(0, 50);
-  noFill();
-  t = 0;
+  background(160, 220, 9);
 }
 
 function draw() {
-  translate(width / 3, height / 2);
-  beginShape();
-  for (let i = 0; i < 130; i++) {
-    let ang = map(i, 0, 100, 0, 10);
-    let rad = 500 * noise(i * 0.1, t * 0.005);
-    let x = rad * cos(ang);
-    let y = rad * sin(ang);
-    curveVertex(x, y);
-  }
-  endShape(CLOSE);
+  // background(0, 10);
+  let posX1 = map(noise(noiseTime), 0, 1, 0, windowWidth);
+  let posY1 = map(noise(noiseTime + 8), 0, 1, 0, windowHeight);
+  let posX2 = map(noise(noiseTime + 10), 0, 1, 0, windowWidth);
+  let posY2 = map(noise(noiseTime + 52), 0, 1, 0, windowHeight);
 
-  t += 1;
+  let tam1 = map(noise(noiseTam + 50), 0, 1, 120, 1);
+  let tam2 = map(noise(noiseTam + 240), 0, 1, 90, 1);
 
-  if (frameCount % 1000 == 0) {
-    background(255);
-  }
+  noiseTime += 0.001;
+  noiseTam += 0.05;
+
+  stroke(20, 80, 60, 10);
+  strokeWeight(4);
+
+  fill(255);
+  //noStroke();
+  circle(posX1, posY1, tam1);
+  fill(10, 205, 50);
+  circle(posX2, posY2, tam2);
 }
